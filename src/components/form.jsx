4f309a913bdb564d7c276045f479d700b779f1e3
Ghulam Subbhani <gsmalik030@gmail.com>
2023-02-21 09:43:57 +0200

    Fixed eslint errors

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,35 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const AddTodo = (props) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
 
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-
-   const submit=(e)=>{
+  const submit = (e) => {
     e.preventDefault();
-    if(!title || !description){
-        alert("Title and Description is Required!")
+    if (!title || !description) {
+      alert('Title and Description is Required!');
+    } else {
+      props.addTodo(title, description);
+      setTitle('');
+      setDescription('');
     }
-    else{
-        props.addTodo(title, description);
-    setTitle("");
-    setDescription("");
-    }
-   } 
+  };
   return (
-    <form className='container' onSubmit={submit}>
-        <h3 className='text-center'>Add Todo</h3>
-  <div className="mb-3">
-    <label htmlFor="title" className="form-label">Title</label>
-    <input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}} className="form-control" id="title"  />
-  </div>
-  <div className="mb-3">
-    <label htmlFor="description" className="form-label">description</label>
-    <input type="text" value={description} onChange={(e)=>{setDescription(e.target.value)}} className="form-control" id="description" />
-  </div>
-  <button type="submit" className="btn btn-success btn-sm">Add Todo</button>
-</form>
-  )
-}
+    <form className="container" onSubmit={submit}>
+      <h3 className="text-center">Add Todo</h3>
+      <div className="mb-3">
+        <label htmlFor="title" className="form-label">Title</label>
+        <input type="text" value={title} onChange={(e) => { setTitle(e.target.value); }} className="form-control" id="title" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="description" className="form-label">description</label>
+        <input type="text" value={description} onChange={(e) => { setDescription(e.target.value); }} className="form-control" id="description" />
+      </div>
+      <button type="submit" className="btn btn-success btn-sm">Add Todo</button>
+    </form>
+  );
+};
 
-export default AddTodo
+export default AddTodo;
