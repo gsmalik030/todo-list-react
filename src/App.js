108ca313c0fc628b3cad6449de48907d2a3b1051
Ghulam Subbhani <gsmalik030@gmail.com>
2023-02-21 08:25:27 +0200

    Fixed linters errors

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
 import './App.css';
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import Header from './components/header';
 import Todos from './components/todos';
 import AddTodo from './components/form';
+
 function App() {
-
   let initTodo;
-  if(localStorage.getItem('todos')=== null){
-    initTodo =[];
+  if (localStorage.getItem('todos') === null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem('todos'));
   }
-  else{
-    initTodo = JSON.parse(localStorage.getItem('todos'))
-  }
+  const [todos, setTodos] = useState(initTodo);
 
-  const handleDelete= (todo)=>{
-    setTodos(todos.filter( e =>{
-      return e!==todo;
-    }))
-    localStorage.setItem('todos', JSON.stringify(todos))
-  }
-
-  const handleAddTodo=(title, description)=>{
-    let sn = todos.length + 1;
+  const handleAddTodo = (title, description) => {
+    const sn = todos.length + 1;
     const newTodo = {
-      sn,title,description
-    }
-    setTodos([...todos, newTodo])
-  }
-  const [todos,setTodos] =useState(initTodo)
-  useEffect(()=>{
-    localStorage.setItem('todos', JSON.stringify(todos))
-  },[todos])
+      sn,
+      title,
+      description,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const handleDelete = (todo) => {
+    setTodos(todos.filter((e) => e !== todo));
+    localStorage.setItem('todos', JSON.stringify(todos));
+  };
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
   return (
-    <React.Fragment>
-    <Header title={"Todos List"}/>
-    <AddTodo addTodo= {handleAddTodo} />
-    <Todos todos={todos} onDelete={handleDelete} />
-    </React.Fragment>
+    <>
+      <Header title="Todos List" />
+      <AddTodo addTodo={handleAddTodo} />
+      <Todos todos={todos} onDelete={handleDelete} />
+    </>
   );
 }
 
